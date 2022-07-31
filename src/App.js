ed39449a30fec49530cd37704fcc9eafe4845ae7
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-07-31 09:35:49 -0400

    commented out key in app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import './App.css';
-import { useState } from 'react'
-import MoleContainer from './components/MoleContainer';
 
+import { useState } from 'react'
+import MoleContainer from "./components/MoleContainer"
+import './App.css';
 
 function App() {
-  let [score, setScore] = useState(0) 
+  let [score, setScore] = useState(0)
 
   const createMoleHill = () => {
     let hills = []
-    for (let i = 0; i < 9; i++) {
-      hills.push(<MoleContainer key={i} setScore={setScore} score={score} />)
+    for (let i = 0; i < 9; i++){
+      hills.push(
+        <MoleContainer 
+        //key = 
+        setScore={setScore}
+        score={score} />
+      )
     }
-    return (
+     return (
+    
       <div>
         {hills}
       </div>
-    )
+    
+  );
   }
-
   return (
-    <div className='App'>
-      <h1>React-A-Mole!</h1>
-      {score}
-      {createMoleHill}
+    <div className="App">
+      <div>
+        {score}
+        {createMoleHill()}
+      </div>
     </div>
   );
 }
-
-
 
 export default App;
